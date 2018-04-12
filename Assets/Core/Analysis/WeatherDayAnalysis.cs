f8578bd9e480711dce60070daf231a2ed6b551fe
zouhangte <zouhantezbm@126.com>
2018-04-12 19:21:12 +0800

    更新demo

--- Assets/Core/Analysis/WeatherDayAnalysis.cs
+++ Assets/Core/Analysis/WeatherDayAnalysis.cs
@@ -3,10 +3,31 @@ using UnityEngine.UI;
 using UnityEngine.Events;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
 namespace Weather
 {
     public class WeatherDayAnalysis : WeatherAnalysis
     {
+        #region 当前
+        public string temperature;
+        public string humidity;
+        public string airQuality;
+        public string wind1;
+        public string wind2;
+        public WeatherStatus weatherStatus;
+        #endregion
+
+        #region 一天
+        public string[] times;
+        public string[] temperatures;
+        public string[] wind1s;
+        public string[] wind2s;
+        public WeatherStatus[] weatherStatuses;
+        #endregion
+
+        private UnityAction onComplete;
         private string url
         {
             get
@@ -17,7 +38,29 @@ namespace Weather
         }
         public WeatherDayAnalysis(string provinceID, string cityID, string districtID) : base(provinceID, cityID, districtID) { }
 
+        public void UpdateAsync(UnityAction onComplete)
+        {
+            this.onComplete = onComplete;
+            AsyncUtil.StartCoroutine(UpdateData());
+        }
+        IEnumerator UpdateData()
+        {
+            var www = new WWW(url);
+            yield return www;
+            AnalysisData(www.bytes);
+        }
 
+        private void AnalysisData(byte[] contentBytes)
+        {
+            using (MemoryStream stream = new MemoryStream(contentBytes))
+            using (StreamReader reader = new StreamReader(stream,System.Text.Encoding.UTF8))
+            {
+                string content = reader.ReadToEnd();
+
+               
+            }
+
+        }
     }
 }
 
---
+++ Assets/Demo/DayWeather/oneDay.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections;
+using System.Collections.Generic;
+using Weather;
+
+public class oneDay : MonoBehaviour {
+    private void Start()
+    {
+        WeatherDayAnalysis analysis = new WeatherDayAnalysis("10101", "00", "01");
+        analysis.UpdateAsync(() => {
+            Debug.Log("温度：" + analysis.temperature);
+        });
+    }
+}
